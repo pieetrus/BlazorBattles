92cc7daec4f49728cc8a852afdcddc7f8b92d27b
Jakub Pietrus <jakub.pietrus@tasksoftware.com>
2022-05-20 01:02:01 +0200

    Part 2

--- BlazorBattles/Client/Program.cs
+++ BlazorBattles/Client/Program.cs
@@ -1,5 +1,6 @@
 using BlazorBattles.Client;
 using BlazorBattles.Client.Services;
+using Blazored.Toast;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,7 +8,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredToast();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IBananaService, BananaService>();
+builder.Services.AddScoped<IUnitService, UnitService>();
 
 await builder.Build().RunAsync();
